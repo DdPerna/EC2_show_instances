57d476f34d68ca3c13f33c7d4502774a8fb49e88
Didave <dedipern@gmail.com>
2016-09-26 10:28:17 -0400

    Update show_instances.sh

--- show_instances.sh
+++ show_instances.sh
@@ -31,7 +31,7 @@ if [ "$1" == "-h" ]; then
   echo " -h     display help "
   echo ""
   echo "### Options ####"
-  echo "Region - {us-east-1,us-west-1}"
+  echo "Region - {us-east-1 us-west-1 us-west-2 ap-south-1 ap-northeast-2 ap-southeast-1 ap-southeast-2 ap-northeast-1 eu-central-1 eu-west-1 sa-east-1}"
   echo "Environment - {Prod,Pilot,Stage}"
   echo "Type - {LOG,HTTPD,APP}"
   echo ""
